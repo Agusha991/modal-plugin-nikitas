477cf8aa17a8914020f78cfa74efb71733ce69ba
nikita <nikita.a@halykmarket.com>
2025-01-24 17:29:51 +0500

    updated: remove version plugin from dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,27 @@
-import { App, ref } from 'vue';
+import { App } from 'vue';
 import Modal from './components/Modal.vue';
 
 export default {
     install(app: App) {
+        // Register the Modal component globally
         app.component('Modal', Modal);
         console.log('agusha_nikita plugin initialized!');
+
+        // Modal methods
         const modalMethods = {
             open(name: string, options: Record<string, any> = {}) {
                 console.log(`Opening modal: ${name}`, options);
-                // Your modal opening logic here
             },
             close(id: number) {
                 console.log(`Closing modal: ${id}`);
-                // Your modal closing logic here
             },
         };
+
         // Make $modal globally accessible
         app.config.globalProperties.$modal = modalMethods;
+        console.log('modalMethods have been added to globalProperties');
     },
 };
-
 declare module '@vue/runtime-core' {
     interface ComponentCustomProperties {
         $modal: {
