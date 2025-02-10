9cf493c5d7b8fe915fcab4ab29c076e8b85e121f
Tokhtar Aubakirov <tokhtar.aubakirov@ffins.kz>
2025-02-10 14:53:05 +0500

    fix: file icons name change to CamelCase && in modal component wrap getTypeModal by computed property

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,69 +1,71 @@
 <template>
   <div class="modal-container" @click.stop>
     <div
-        class="modal-container-header"
-        :class="{'modal-container-header-show-all': showMore, [getTypeModal().nameClass]: true}"
+      class="modal-container-header"
+      :class="{
+        'modal-container-header-show-all': showMore,
+        [getTypeModal.nameClass]: getTypeModal.nameClass !== '',
+      }"
     >
       <div class="modal-container-header-title">
-        <component :is="getTypeModal().img"/>
+        <component :is="getTypeModal.img" />
         <div>{{ options.title }}</div>
       </div>
-      <div class="modal-container-header-show-more" @click="showMore = !showMore">
-        {{ showMore ? 'Скрыть' : 'Смотреть больше' }}
+      <div
+        class="modal-container-header-show-more"
+        @click="showMore = !showMore"
+      >
+        {{ showMore ? "Скрыть" : "Смотреть больше" }}
       </div>
     </div>
-    <CloseIcon class="modal-button-close" @click="onClose()"/>
+    <CloseIcon class="modal-button-close" @click="onClose" />
     <div v-if="showMore" class="modal-container-subtitle">
-      Описание: <br/>
+      Описание: <br />
       {{ options.message }}
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-
-import {ref} from "vue";
-import CloseIcon from "@/components/icons/Close-icon.vue";
-import ErrorIcon from "@/components/icons/Error-icon.vue";
-import SuccessIcon from "@/components/icons/Success-icon.vue";
-import WarningIcon from "@/components/icons/Warning-icon.vue";
-import InfoIcon from "@/components/icons/Info-icon.vue";
+import { ref, computed } from "vue";
+import CloseIcon from "@/components/icons/CloseIcon.vue";
+import ErrorIcon from "@/components/icons/ErrorIcon.vue";
+import SuccessIcon from "@/components/icons/SuccessIcon.vue";
+import WarningIcon from "@/components/icons/WarningIcon.vue";
+import InfoIcon from "@/components/icons/InfoIcon.vue";
 
 interface IProps {
   options: {
     title: string;
     message: string;
-  },
-  name: string,
-  onClose: Function
+  };
+  name: string;
+  onClose: () => void;
 }
 
-const props = defineProps<IProps>()
-const showMore = ref<boolean>(false)
+const props = defineProps<IProps>();
+const showMore = ref<boolean>(false);
 
-
-const getTypeModal = (): { img: any, nameClass: string } => {
+const getTypeModal = computed<{ img: any; nameClass: string }>(() => {
   switch (props.name) {
-    case 'error':
-      return {img: ErrorIcon, nameClass: 'error'}
-    case 'success':
-      return {img: SuccessIcon, nameClass: 'success'}
-    case 'warning':
-      return {img: WarningIcon, nameClass: 'warning'}
-    case 'info':
-      return {img: InfoIcon, nameClass: 'info'}
+    case "error":
+      return { img: ErrorIcon, nameClass: "error" };
+    case "success":
+      return { img: SuccessIcon, nameClass: "success" };
+    case "warning":
+      return { img: WarningIcon, nameClass: "warning" };
+    case "info":
+      return { img: InfoIcon, nameClass: "info" };
     default:
       return {
         img: SuccessIcon,
-        nameClass: ''
-      }
+        nameClass: "",
+      };
   }
-}
-
+});
 </script>
 
 <style lang="scss">
-
 .modal {
   position: fixed;
   top: 0;
@@ -101,7 +103,7 @@ const getTypeModal = (): { img: any, nameClass: string } => {
       font-size: 20px;
       font-weight: 400;
       line-height: 24px;
-      &-show-all{
+      &-show-all {
         border-radius: 12px 12px 0 0;
       }
 
@@ -113,7 +115,7 @@ const getTypeModal = (): { img: any, nameClass: string } => {
       }
 
       &-show-more {
-        color: #F5C9C3;
+        color: #f5c9c3;
         text-align: right;
         font-size: 16px;
         cursor: pointer;
@@ -129,21 +131,19 @@ const getTypeModal = (): { img: any, nameClass: string } => {
   }
 }
 
-
 .error {
-  background: #EE5542;
+  background: #ee5542;
 }
 
 .success {
-  background: #15B853;
+  background: #15b853;
 }
 
 .warning {
-  background: #F4A900;
+  background: #f4a900;
 }
 
 .warning {
-  background: #0085CF;
+  background: #0085cf;
 }
-
 </style>
