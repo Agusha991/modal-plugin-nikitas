aa568100874ed4ab37332a3ba44ef17b1e786c4f
nikita <nikita.a@halykmarket.com>
2025-02-14 11:08:20 +0500

    feature: changed version plugin, added new params for webpack.config.js and change added new component to global access

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,19 @@ module.exports = {
     entry: './src/index.ts',
     mode: "none",
     output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
-        library: 'nikitaModal', //name for import to boot file
-        libraryTarget: 'umd',
-        umdNamedDefine: true,
+        path: __dirname + "/dist",
+        filename: "index.js",
+        libraryTarget: "umd", // Universal Module Definition
+        library: "ModalPluginNikitas",
+        globalObject: "this"
     },
+    // output: {
+    //     filename: 'index.js',
+    //     path: path.resolve(__dirname, 'dist'),
+    //     library: 'nikitaModal', //name for import to boot file
+    //     libraryTarget: 'umd',
+    //     umdNamedDefine: true,
+    // },
     resolve: {
         extensions: ['.js', '.ts', '.vue', '.json'],
         alias: {
