ba8f35c14e5b13a7d85b6c3ef53ee07b618437e3
nikita <nikita.a@halykmarket.com>
2025-05-05 15:59:01 +0500

    edit version

--- src/plugins/modalPlugin.ts
+++ src/plugins/modalPlugin.ts
@@ -1,81 +1,82 @@
-import { createApp, h, getCurrentInstance } from "vue";
-import { useModalStore } from "../store/modal";
+import {createApp, h, getCurrentInstance} from "vue";
+import {useModalStore} from "../store/modal";
 import Modal from "../components/Modal/Modal.vue";
-import { watch } from "vue";
-import { IModalState } from "types";
+import {watch} from "vue";
+import {IModalState} from "types";
 
 export default {
-  install(app: any) {
-    const modalStore = useModalStore();
+    install(app: any) {
+        const modalStore = useModalStore();
 
-    // Добавление глобального метода $modal
-    app.config.globalProperties.$modal = {
-      open(name: string, options: IModalState) {
-        modalStore.openModal(name, options); // Открытие модалки через Pinia
-        console.log("Current modals:", modalStore.modals);
-      },
-      close(id: number) {
-        modalStore.closeModal(id); // Закрытие модалки через Pinia
-      },
-    };
-
-    // Найдем элемент с классом container
-    const modalContainer = document.createElement("div");
-    document.body.appendChild(modalContainer);
-
-    // Реактивное обновление модалок через watch
-    watch(
-      () => modalStore.modals,
-      (newModals) => {
-        // Очищаем контейнер
-        modalContainer.innerHTML = "";
-
-        // Добавляем модалки в DOM
-        newModals.forEach((modal) => {
-          modalContainer.dataset.modalId = String(modal.id);
-
-          modalContainer.addEventListener("click", (event) => {
-            console.log(`Closing modal with id: ${modal.id}`);
-            modalStore.closeModal(modal.id);
-          });
-          modalContainer.setAttribute("name", modal.name);
-          modalContainer.classList.add("modal");
-          modalContainer.addEventListener("click", () => {
-            console.log("this click on background");
-            modalStore.closeModal(modal.id);
-          });
-
-          const modalApp = createApp({
-            render() {
-              return h(Modal, {
-                options: modal.options,
-                name: modal.name,
-                onClose: () => {
-                  modalContainer.classList.remove("modal");
-                  modalStore.closeModal(modal.id);
-                },
-              });
+        // Добавление глобального метода $modal
+        app.config.globalProperties.$modal = {
+            open(name: string, options: IModalState) {
+                modalStore.openModal(name, options); // Открытие модалки через Pinia
+                console.log("Current modals:", modalStore.modals);
             },
-          });
-          modalApp.mount(modalContainer);
-        });
-      },
-      { deep: true }
-    );
-  },
+            close(id: number) {
+                modalStore.closeModal(id); // Закрытие модалки через Pinia
+            },
+        };
+
+        // Найдем элемент с классом container
+        const modalContainer = document.createElement("div");
+        document.body.appendChild(modalContainer);
+
+        // Реактивное обновление модалок через watch
+        watch(
+            () => modalStore.modals,
+            (newModals) => {
+                // Очищаем контейнер
+                modalContainer.innerHTML = "";
+
+                // Добавляем модалки в DOM
+                newModals.forEach((modal) => {
+                    modalContainer.dataset.modalId = String(modal.id);
+
+                    modalContainer.addEventListener("click", (event) => {
+                        console.log(`Closing modal with id: ${modal.id}`);
+                        console.log(`Closing modal with id: `, modalStore.modals);
+                        modalStore.closeModal(modal.id);
+                    });
+                    modalContainer.setAttribute("name", modal.name);
+                    modalContainer.classList.add("modal");
+                    modalContainer.addEventListener("click", () => {
+                        console.log("this click on background");
+                        modalStore.closeModal(modal.id);
+                    });
+
+                    const modalApp = createApp({
+                        render() {
+                            return h(Modal, {
+                                options: modal.options,
+                                name: modal.name,
+                                onClose: () => {
+                                    modalContainer.classList.remove("modal");
+                                    modalStore.closeModal(modal.id);
+                                },
+                            });
+                        },
+                    });
+                    modalApp.mount(modalContainer);
+                });
+            },
+            {deep: true}
+        );
+    },
 };
 
 export function useModal() {
-  const instance = getCurrentInstance();
-  return instance?.appContext.config.globalProperties.$modal;
+    const instance = getCurrentInstance();
+    return instance?.appContext.config.globalProperties.$modal;
 }
 
 // Расширение глобального интерфейса для TypeScript
 declare module "@vue/runtime-core" {
-  interface ComponentCustomProperties {
-    $modal: {
-      open: (name: string, options?: Record<string, any>) => void;
-      close: (id: number) => void;
-    };
-  }
+    interface ComponentCustomProperties {
+        $modal: {
+            open: (name: string, options?: Record<string, any>) => void;
+            close: (id: number) => void;
+        };
+    }
 }
